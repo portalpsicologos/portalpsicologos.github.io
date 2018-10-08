6aec9b34ab115b7e2ae5f249fc5433c25aa01060
Diego Canales <dreg27@gmail.com>
2018-10-08 14:37:03 -0300

    fix page in mobile

--- js/active.js
+++ js/active.js
@@ -11,14 +11,6 @@
         }, 800);
     });
 
-    $window.on('scroll', function() {
-        if ($window.scrollTop() > 0) {
-            $('.header_area').addClass('sticky');
-        } else {
-            $('.header_area').removeClass('sticky');
-        }
-    });
-
     $window.on('load', function() {
         $('#preloader').fadeOut('slow', function() {
             $(this).remove();
